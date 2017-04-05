3a1a4e8d3132a233ce7fe2a6eba2c87dec9d48d5
sid <siddharthgovindbhai.parmar@my.jcu.edu.au>
2017-04-05 11:15:55 +0800

    sandbox

---
+++ broken score.py
@@ -0,0 +1,21 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+
+
+def getScore(score):
+    if score < 0:
+        return("Invalid score")
+    elif score > 100:
+        return("Invalid score")
+    elif score > 90:
+        return ("Excellent")
+    elif score >= 50:
+        return ("Passable")
+    else:
+        return ("Bad")
+score = float(input("Enter score: "))
+
+print(getScore(score))
